8b674fd307e5f97b82a06e111b7ac3702c7567e3
Abhishek Tilkar <tilkarabhishek2000@gmail.com>
2025-06-08 00:18:11 +0530

    twilio

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { TwilioService } from './twilio/twilio.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private twilioService: TwilioService) {}
+
+    sendOtp(phoneNumber: string) {
+        return this.twilioService.sendOtp(phoneNumber);
+    }
+}
