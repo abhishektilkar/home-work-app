118c983611fc34e93ee0539e1b6a755b2ae91186
Abhishek Tilkar <tilkarabhishek2000@gmail.com>
2025-06-11 00:22:21 +0530

    jwt access token changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TwilioService } from './twilio/twilio.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.AUTH_SECRET,
+      signOptions: { expiresIn: '11160s' },
+    })
+  ],
   providers: [AuthService, TwilioService],
   controllers: [AuthController]
 })
